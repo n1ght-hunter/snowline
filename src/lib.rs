fa16e0774c65070cd5369c42932841b5adffc9ba
Night_Hunter <samuelhuntnz@gmail.com>
2025-08-29 01:48:54 +1200

    Reorder module declarations and update prelude exports for line_graph

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,12 @@
 //!
 //!
 
-pub mod line_graph;
 pub mod bar_graph;
+pub mod line_graph;
 pub mod zoom;
 
 pub mod prelude {
-    pub use crate::line_graph::{Interaction, LineGraph, LineGraphState};
     pub use crate::bar_graph::{BarGraph, BarGraphState};
+    pub use crate::line_graph::{LineGraph, LineGraphState};
     pub use crate::zoom::Zoom;
 }
